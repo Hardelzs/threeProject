3181b2da33b06f9378605337ae99f05e3e431510
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-03-04 23:24:26 +0100

    vvv

--- src/main.js
+++ src/main.js
@@ -3,15 +3,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup Scene
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#bg'), antialias: true });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-
-renderer.render(scene, camera);
 
 // Grid Helper & Axes Helper for better 3D perspective
 const gridHelper = new THREE.GridHelper(200, 50);
